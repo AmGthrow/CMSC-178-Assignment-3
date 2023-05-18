855ecab52bc06cda3e55ea9134391ea793863585
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-18 10:24:04 +0800

    implement 3A solution

--- A3_code/A3_code/hitormiss_3x3.m
+++ A3_code/A3_code/hitormiss_3x3.m
@@ -9,8 +9,9 @@ end
 
 % --------------- INSERT YOUR CODE BELOW -----------------
 
-% note use the erode_3x3 function call as part of your solution
-Imatch = rand(size(I)) > 0.9;
+B1 = M == 1; % Get set pixels
+B2 = M == -1; % Get unset pixels
+Imatch = erode_3x3(I, B1) & erode_3x3(~I, B2);
 
 % --------------- INSERT YOUR CODE ABOVE -----------------
 
@@ -21,7 +22,23 @@ return
 
 function E = erode_3x3(I,S)
 % --------------- INSERT YOUR CODE BELOW -----------------
-E = I | (rand(size(I)) > 0.9);
+
+[HEIGHT, WIDTH] = size(I);
+E = zeros(HEIGHT, WIDTH);
+for y=2:HEIGHT-1 % Avoid borders of image, assume S is 3x3
+    for x=2:WIDTH-1
+        top = y-1;
+        bottom = y+1;
+        left = x-1;
+        right = x+1;
+        Isubset = I(top:bottom, left:right);
+        match = Isubset & S;        
+
+        E(y,x) = isequal(match, S); % Ensure all values in filter are present in image
+    end
+end
+
+
 % --------------- INSERT YOUR CODE ABOVE -----------------
 return
 
