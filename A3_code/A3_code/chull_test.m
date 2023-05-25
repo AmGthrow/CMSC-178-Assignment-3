38471041effee91c8889e666de6d6a9189623fac
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-25 09:57:13 +0800

    Add extra test in convex-hull to see L-shaped structures

--- A3_code/A3_code/chull_test.m
+++ A3_code/A3_code/chull_test.m
@@ -23,3 +23,41 @@ subplot(2,2,4);
 imagesc( CHinf + I );
 title('CHull inf-steps + Original');
 drawnow
+
+
+% TEST 2 ---- see L-shapes clearly
+figure;
+I = [
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0;
+    0 1 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0;
+    0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0;
+    0 0 0 0 0 0 1 1 0 0 1 0 0 1 0 0 0;
+    0 0 0 1 0 0 1 1 0 0 1 0 1 1 1 0 0;
+    0 0 0 1 0 0 0 0 0 0 0 0 0 1 0 0 0;
+    0 0 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0;
+    0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0;
+    0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0;
+    ];
+
+subplot(2,2,1);
+imagesc(I); colormap(gray);
+title('Binary image');
+
+CH1 = chull_3x3(I,1);
+subplot(2,2,2);
+imagesc( CH1 + I );
+title('CHull 1-step + Original'); 
+drawnow
+
+CH5 = chull_3x3(I,5);
+subplot(2,2,3);
+imagesc( CH5 + I );
+title('CHull 5-steps + Original');
+drawnow
+
+CHinf = chull_3x3(I,inf);
+subplot(2,2,4);
+imagesc( CHinf + I );
+title('CHull inf-steps + Original');
+drawnow
