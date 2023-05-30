fd8ed3b3f51bc99503f88a9e6cd0ca45e524cfe5
sycasec <kadelcastillo@up.edu.ph>
2023-05-31 03:40:11 +0800

    finished log_edge

--- A3_code/A3_code/log_edge.m
+++ A3_code/A3_code/log_edge.m
@@ -26,16 +26,49 @@ threshK=max(1,-0.5*N+7.5); % multiply threshold by this factor
 
 %-----------change code from here --------------------------------
 %
-% 1. Create and emtpy array E and and an array G containing the filtered
+% 1. Create and empty array E and and an array G containing the filtered
 %    image (use conv2 with the 'same' option to do this).
+E = zeros(size(I));
+G = conv2(I, F, 'same');
+
 % 2. compute threshold t (0.75*mean(G)) of the LoG image stored in
 %    G (and multiply by threshK)
+t = 0.75 * mean(abs(G(:))) * threshK;
+
 % 3. identify the zero crossing points 
 % 4. preserve those zero crossing points where the sum of the
 %    magnitudes of G accross the zero crossing is > t
 
-% NOTE: remove the dummy code line below
-E = rand(size(I)) > 0.95; G=[]; % <-- remove
+% Get half of the distance from the center based on the size of the filter 
+halfDist = floor(N/2);
+
+% Create array so that checking each side will be easier
+sides = [0 1; 0 -1; 1 0; -1 0];
+[rowSide, ~] = size(sides);
+
+% Iterate through image, except on edges. 
+[y,x] = size(G);
+for i = halfDist:y-halfDist
+    for j = halfDist:x-halfDist
+        
+        % Check if current pixel is positive
+        currPixel = G(i,j);
+        if currPixel > 0
+             
+            % If positive, check if one side is negative. If so, a zero
+            % crossing is present and is indicated in the edge image.
+            for k = 1:rowSide
+                edge = G(i + sides(k,1),j + sides(k,2));
+                if edge < 0
+                    if abs(currPixel) + abs(edge) > t
+                        E(i,j) = 1;
+                        break
+                    end
+                end
+            end     
+        end
+    end
+end
 
 %-----------change code above here --------------------------------
 
